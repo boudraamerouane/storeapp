697e89b396c7ee3daa3d5ef8fdfd3d3a49f4e75b
boudraamarwane <merouaneboudraa98@gmail.com>
2025-10-04 13:49:53 +0100

    Fix DB

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,10 +68,11 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 # Database (keep SQLite for now, can switch to Postgres on Render)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
